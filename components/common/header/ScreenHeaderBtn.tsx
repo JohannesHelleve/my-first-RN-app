c8cae0337d14539fb47ecb7c31fec72a35e14c15
Johannes Helleve <Johanshelleve@gmail.com>
2023-12-18 12:31:11 +0100

    changed to tsx, and type declarations

---
+++ components/common/header/ScreenHeaderBtn.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './screenheader.style'
+
+type ScreenHeaderBtnProps = {
+  iconUrl: string,
+  dimension: string
+}
+
+const ScreenHeaderBtn = ({ iconUrl, dimension }: ScreenHeaderBtnProps) => {
+  asdfdfkljkkasdlkjkækfs
+
+  fasfdkkjjkasdfa-flip-horizontal
+
+  adsdlllasdfjjjjjaskdlføas
+  return (
+    <View>
+      <Text>ScreenHeaderBtn</Text>
+    </View>
+  )
+}
+
+export default ScreenHeaderBtn
