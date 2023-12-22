0de02b0285858a73d58ffe0867d7c31d138c9ba2
Sjefen <Johanshelleve@gmail.com>
2023-12-22 15:58:53 +0100

    trøbbel med flatlist

--- components/home/welcome/Welcome.tsx
+++ components/home/welcome/Welcome.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'expo-router'
 import { styles, tab, tabText } from './welcome.style'
 import { icons, SIZES } from '../../../constants' 
 
+const jobTypes = ["Fulltid", "Deltid", "Intern"]
+
 const Welcome = () => {
   const router = useRouter()
 
@@ -23,7 +25,18 @@ const Welcome = () => {
             placeholder="Søk etter en jobb"
           />
         </View>
-
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer} >
+        <FlatList  
+          data={jobTypes}
+        />
       </View>
     </View>
   )
