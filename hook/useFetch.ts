9b404c5ee01a3a1b1292d5965476da2e1df5a876
Sjefen <Johanshelleve@gmail.com>
2024-01-04 21:09:19 +0100

    for mye støy

--- hook/useFetch.ts
+++ hook/useFetch.ts
@@ -4,24 +4,46 @@ import { RAPID_API_KEY } from '@env';
 
 
 const rapidApiKey = RAPID_API_KEY;
- k
-const useFetch = (endpoint : string) => {
+
+
+const useFetch = (endpoint : string, query : object) => {
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
     const options = {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-        params: {
-          query: 'Python developer in Texas, USA',
-          page: '1',
-          num_pages: '1'
-        },
+        params: { ...query },
         headers: {
           'X-RapidAPI-Key': rapidApiKey,
           'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         }
       };
+
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try{
+        const response = await axios.request(options);
+        setData(response.data.data);
+        setIsLoading(false);
+      } catch (error : any) {
+        setError(error);
+        alert('Error fetching data');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+    const refetch = () => {
+      setIsLoading(true);
+      fetchData();
+     }
+
+    return { data, isLoading, error, refetch }
 
 }
